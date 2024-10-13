aad7fed1fb35e843ca6a56e25b11b808c9ab2760
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-13 17:43:52 +0000

    rock,paper,scissors paths completed

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" #only for developing remove when deploying app
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home, {:layout => :layout})
+end
+
+get("/rock") do
+  @moves=["rock", "paper", "scissors"]
+  @comp_move = @moves.sample
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  end
+
+  erb(:rock, {:layout => :layout})
+end
+
+get("/scissors") do
+  @moves=["rock", "paper", "scissors"]
+  @comp_move = @moves.sample
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  end
+
+  erb(:scissors, {:layout => :layout})
+end
+
+get("/paper") do
+  @moves=["rock", "paper", "scissors"]
+  @comp_move = @moves.sample
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  end
+
+  erb(:paper, {:layout => :layout})
 end
